396adf8c866738f90b709cbf8a50a0ce4cec402b
2110315 <ichamude@gmail.com>
2025-05-20 20:23:27 +0800

    Add files via upload

---
+++ pertemuan10/pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,34 @@
+
+package pertemuan10;
+
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author LAB F
+ * TGL:20 Mei 2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        dbkoneksi cnn = new dbkoneksi();
+       Statement st = cnn.koneksi().createStatement();
+       
+       String sql = "SELECT * FROM mhs;";
+       ResultSet res = st.executeQuery(sql);
+       
+       
+       while(res.next()){
+           
+           System.out.println("Nama: "+res.getString("NAMA"));
+       }
+       
+        // TODO code application logic here
+    }
+    
+}
